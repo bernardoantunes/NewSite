953b9e5a58979082c23fa0f5fe70610f38ce8c60
Bernardo Antunes <myself@bernardoantunes.com>
2014-04-20 04:37:23 -0700

    Added the new package file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 http.createServer(function(request, response) {
 
     response.writeHead(200, { "Content-Type": "text/plain" });
-    response.write("Hello World");
+    response.write("Hello World being monitored! :D");
     response.end();
 
 }).listen(8888);
